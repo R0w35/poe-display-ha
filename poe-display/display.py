10eb912b6026cacb9195aa2e98c76bb0972bb9e1
R0w35 <147671159+R0w35@users.noreply.github.com>
2026-04-11 01:28:11 +1200

    Update display.py

--- poe-display/display.py
+++ poe-display/display.py
@@ -12,7 +12,7 @@ serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, width=128, height=32)
 
 try:
-    font = ImageFont.truetype("ProggySquare.ttf", 16)
+    font = ImageFont.truetype("ProggyTiny.ttf", 16)
 except:
     print("Proggy font not found, falling back to default")
     font = ImageFont.load_default()
